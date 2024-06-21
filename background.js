b47fb0cd74919aff00b86e3f64c22b0c3c16a549
Roustem Karimov <104145+roustem@users.noreply.github.com>
2024-06-21 17:12:20 -0400

    Add support for Fastmail

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-console.log("Background script running...");
+console.log("FilterMLT is running...");
 
 chrome.commands.onCommand.addListener(function (command) {
   console.log("onCommand");
@@ -21,23 +21,70 @@ chrome.action.onClicked.addListener((tab) => {
 });
 
 function filterMessages() {
-  console.log("Filtering messages from sender...");
-  let main = document.querySelector("div[role=main]");
-  if (main) {
-    let el = main.querySelector("span[email]");
-    if (el) {
-      let senderEmail = el.getAttribute("email");
-      let querySearch = "from:(" + senderEmail + ")";
+  // mail.google.com
+  function filterGmail(baseUrl) {
+    let main = document.querySelector("div[role=main]");
+    if (main) {
+      let el = main.querySelector("span[email]");
+      if (el) {
+        let senderEmail = el.getAttribute("email");
+        let querySearch = "from:(" + senderEmail + ")";
+        let queryUrl = baseUrl + "#search/" + querySearch;
 
-      let match = window.location.href.match(
-        /(https:\/\/mail\.google\.com\/mail\/u\/\d+\/)/
-      );
-      if (match) {
-        var queryUrl = match[0] + "#search/" + querySearch;
+        console.log("Redirecting to: " + queryUrl);
         window.location.href = queryUrl;
-      } else {
-        console.warn("Failed to parse current mail.google.com URL");
       }
     }
   }
+
+  // app.fastmail.com
+  function filterFastmail(baseUrl) {
+    let h2 = document.querySelector("h2.v-MessageCard-from");
+    if (h2 && h2.title) {
+      let composeButton = document.querySelector("a.s-new-message");
+      if (composeButton && composeButton.href) {
+        let userId = composeButton.href.match(/\?u=(\d+)/)[1];
+        let queryUrl =
+          baseUrl + "search:" + encodeURIComponent(h2.title) + "/?u=" + userId;
+
+        console.log("Redirecting to: " + queryUrl);
+        window.location.href = queryUrl;
+      } else {
+        console.warn("Compose button not found");
+      }
+    } else {
+      console.warn("Sender email not found");
+    }
+  }
+
+  // betaapp.fastmail.com
+  function filterFastmailBeta(baseUrl) {
+    filterFastmail(baseUrl);
+  }
+
+  console.log("Filtering messages from sender...");
+
+  let href = window.location.href;
+  var m = href.match(/(https:\/\/mail\.google\.com\/mail\/u\/\d+\/)/);
+  if (m) {
+    console.log("Filtering Gmail");
+    filterGmail(m[1]);
+    return;
+  }
+
+  m = href.match(/(https:\/\/app\.fastmail\.com\/mail\/).*/);
+  if (m) {
+    console.log("Filtering Fastmail");
+    filterFastmail(m[1]);
+    return;
+  }
+
+  m = href.match(/(https:\/\/betaapp\.fastmail\.com\/mail\/).*/);
+  if (m) {
+    console.log("Filtering Fastmail Beta");
+    filterFastmailBeta(m[1]);
+    return;
+  }
+
+  console.warn(`Unrecognized email service: ${href}`);
 }
